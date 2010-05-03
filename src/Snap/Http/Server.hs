31e92c2f824e57f3325007a7215a6993d5c14ea0
Gregory Collins <greg@gregorycollins.net>
2010-05-03 00:00:14 -0400

    Haddock for snap-server

--- src/Snap/Http/Server.hs
+++ src/Snap/Http/Server.hs
@@ -1,3 +1,7 @@
+-- | The Snap HTTP server is a high performance, epoll-enabled, iteratee-based
+-- web server library written in Haskell. Together with the @snap-core@ library
+-- upon which it depends, it provides a clean and efficient Haskell programming
+-- interface to the HTTP protocol.
 module Snap.Http.Server
 (
   httpServe
@@ -8,12 +12,12 @@ import           Snap.Types
 import qualified Snap.Internal.Http.Server as Int
 
 
--- | FIXME: document this
+-- | Starts serving HTTP requests on the given port using the given handler.
 httpServe :: ByteString      -- ^ bind address, or \"*\" for all
           -> Int             -- ^ port to bind to
           -> ByteString      -- ^ local hostname (server name)
-          -> Maybe FilePath  -- ^ path to the access log
-          -> Maybe FilePath  -- ^ path to the error log
+          -> Maybe FilePath  -- ^ path to the (optional) access log
+          -> Maybe FilePath  -- ^ path to the (optional) error log
           -> Snap ()         -- ^ handler procedure
           -> IO ()
 httpServe bindAddress bindPort localHostname alog elog handler =
