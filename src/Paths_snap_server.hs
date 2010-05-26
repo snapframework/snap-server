6b5d46a88da919b21e7175ebb6f033fc1cf3be27
Gregory Collins <greg@gregorycollins.net>
2010-05-26 12:08:23 -0400

    Commit dummy paths file

---
+++ src/Paths_snap_server.hs
@@ -0,0 +1,9 @@
+module Paths_snap_server (
+    version
+  ) where
+
+import Data.Version (Version(..))
+
+version :: Version
+version = Version {versionBranch = [0,0,0], versionTags = ["unknown"]}
+
