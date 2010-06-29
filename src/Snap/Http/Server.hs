e33c38bcd1ef43de9b2addea1ea5dfb62b03efb4
Carl Howells <chowells@others.local>
2010-06-28 22:57:01 -0700

    Do a lot more work on Config support

--- src/Snap/Http/Server.hs
+++ src/Snap/Http/Server.hs
@@ -5,15 +5,23 @@
 module Snap.Http.Server
 (
   httpServe
+, httpServeConfig
+, quickHttpServe
 , snapServerVersion
 ) where
 
+import           Control.Exception (SomeException)
+import           Control.Monad
+import           Control.Monad.CatchIO
+import qualified Data.ByteString.Char8 as S
 import           Data.ByteString (ByteString)
 import           Snap.Types
 import qualified Snap.Internal.Http.Server as Int
+import           Snap.Http.Server.Config
 
 
 ------------------------------------------------------------------------------
+-- | A short string describing the Snap server version
 snapServerVersion :: ByteString
 snapServerVersion = Int.snapServerVersion
 
@@ -33,3 +41,33 @@ httpServe bindAddress bindPort localHostname alog elog handler =
     Int.httpServe bindAddress bindPort localHostname alog elog handler'
   where
     handler' = runSnap handler
+
+
+------------------------------------------------------------------------------
+-- | Starts serving HTTP requests using the given handler, with
+-- settings from the 'Config' passed in.  This function will only
+-- return after being interrupted by an asynchronous exception.
+httpServeConfig :: Config -> Snap () -> IO ()
+httpServeConfig conf handler = do
+    ifNoisy . putStrLn $ "Listening on " ++
+                         (S.unpack $ configBindAddress conf) ++
+                         ":" ++ show (configListenPort conf)
+    _ <- try serve :: IO (Either SomeException ())
+    ifNoisy $ putStrLn " shutting down.."
+  where
+    ifNoisy = when $ configVerbose conf
+    serve = httpServe (configBindAddress conf)
+                      (configListenPort conf)
+                      (configLocalHostname conf)
+                      (configAccessLog conf)
+                      (configErrorLog conf)
+                      handler
+
+
+------------------------------------------------------------------------------
+-- | Starts serving HTTP using the given handler.  The configuration
+-- is picked up from command-line parameters, as returned by
+-- 'readConfigFromCmdLineArgs'.
+quickHttpServe :: Snap () -> IO ()
+quickHttpServe handler =
+    readConfigFromCmdLineArgs >>= flip httpServeConfig handler
