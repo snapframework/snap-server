ef7def97a0f419372f8dda30376108bf24a57b56
Gregory Collins <greg@gregorycollins.net>
2010-09-09 14:42:45 -0400

    Reorg testsuite a little -- put testserver back in, move code to a 'common' area

---
+++ test/common/Test/Common/TestHandler.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Common.TestHandler (testHandler) where
+
+
+import           Control.Monad
+
+import qualified Data.ByteString.Char8 as B
+import           Data.Maybe
+
+import           Snap.Iteratee hiding (Enumerator)
+import           Snap.Types
+import           Snap.Http.Server
+import           Snap.Util.FileServe
+
+import           Snap.Internal.Iteratee.Debug
+
+
+pongHandler :: Snap ()
+pongHandler = modifyResponse $ setResponseBody (enumBS "PONG") .
+                               setContentType "text/plain" .
+                               setContentLength 4
+
+echoUriHandler :: Snap ()
+echoUriHandler = do
+    req <- getRequest
+    writeBS $ rqURI req
+
+
+echoHandler :: Snap ()
+echoHandler = do
+    unsafeDetachRequestBody >>= \e -> do
+      let (SomeEnumerator x) = e
+      let e' i = x (iterateeDebugWrapper "echoHandler" i)
+      modifyResponse $ setResponseBody e'
+
+
+responseHandler :: Snap ()
+responseHandler = do
+    !code <- liftM (read . B.unpack . fromMaybe "503") $ getParam "code"
+    modifyResponse $ setResponseCode code
+    writeBS $ B.pack $ show code
+
+
+testHandler :: Snap ()
+testHandler =
+    route [ ("pong", pongHandler)
+          , ("echo", echoHandler)
+          , ("echoUri", echoUriHandler)
+          , ("fileserve", fileServe "testserver/static")
+          , ("respcode/:code", responseHandler)
+          ]
+
--- test/testserver/Main.hs
+++ test/testserver/Main.hs
@@ -5,18 +5,10 @@
 module Main where
 
 import           Control.Concurrent
-import           Control.Monad
 
-import qualified Data.ByteString.Char8 as B
-import           Data.Maybe
-
-import           Snap.Iteratee hiding (Enumerator)
-import           Snap.Types
 import           Snap.Http.Server
-import           Snap.Util.FileServe
+import           Test.Common.TestHandler
 
-
-import Snap.Internal.Iteratee.Debug
 
 {-
 
@@ -27,41 +19,6 @@ pipelined POST requests
 slowloris attack / timeout test
 
 -}
-
-pongHandler :: Snap ()
-pongHandler = modifyResponse $ setResponseBody (enumBS "PONG") .
-                               setContentType "text/plain" .
-                               setContentLength 4
-
-echoUriHandler :: Snap ()
-echoUriHandler = do
-    req <- getRequest
-    writeBS $ rqURI req
-
-
-echoHandler :: Snap ()
-echoHandler = do
-    unsafeDetachRequestBody >>= \e -> do
-      let (SomeEnumerator x) = e
-      let e' i = x (iterateeDebugWrapper "echoHandler" i)
-      modifyResponse $ setResponseBody e'
-
-
-responseHandler :: Snap ()
-responseHandler = do
-    !code <- liftM (read . B.unpack . fromMaybe "503") $ getParam "code"
-    modifyResponse $ setResponseCode code
-    writeBS $ B.pack $ show code
-
-
-handlers :: Snap ()
-handlers =
-    route [ ("pong", pongHandler)
-          , ("echo", echoHandler)
-          , ("echoUri", echoUriHandler)
-          , ("fileserve", fileServe "testserver/static")
-          , ("respcode/:code", responseHandler)
-          ]
 
 
 main :: IO ()
@@ -76,6 +33,6 @@ main = do
   where
     go m = do
         httpServe "*" 3000 "localhost" (Just "ts-access.log")
-                  (Just "ts-error.log") handlers 
+                  (Just "ts-error.log") testHandler 
         putMVar m ()
 
