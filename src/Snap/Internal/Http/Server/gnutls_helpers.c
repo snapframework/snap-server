b138dc1d8b0a1423ace07f2ff26f925b6d2bac12
Gregory Collins <greg@gregorycollins.net>
2011-08-10 20:25:39 +0200

    Try to properly initialize the gcrypt library.

--- src/Snap/Internal/Http/Server/gnutls_helpers.c
+++ src/Snap/Internal/Http/Server/gnutls_helpers.c
@@ -1,7 +1,9 @@
-#include <gnutls/gnutls.h>
-#include <gcrypt.h>
 #include <errno.h>
+#include <gcrypt.h>
+#include <gnutls/gnutls.h>
 #include <pthread.h>
+#include <stdio.h>
+
 GCRY_THREAD_OPTION_PTHREAD_IMPL;
 
 /* See http://www.gnu.org/software/gnutls/manual/html_node/Multi_002dthreaded-applications.html */
@@ -12,6 +14,11 @@ void gnutls_set_threading_helper()
 {
     if (!threading_init) {
         gcry_control (GCRYCTL_SET_THREAD_CBS, &gcry_threads_pthread);
+        if (!gcry_check_version (GCRYPT_VERSION)) {
+            fputs ("libgcrypt version mismatch\n", stderr);
+            exit (2);
+        }
+        gcry_control (GCRYCTL_INITIALIZATION_FINISHED, 0);
         threading_init = 1;
     }
 }
