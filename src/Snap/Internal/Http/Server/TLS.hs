fa3c07d19ebd50ce3485fb413393702b199434a5
Gregory Collins <greg@gregorycollins.net>
2012-09-19 22:05:44 +0200

    Revert "Desperation tactic: try to force the garbage collector not to kill the"
    This reverts commit bb94339aab4b1f6c964e2977364069137d3f4fbf.

--- src/Snap/Internal/Http/Server/TLS.hs
+++ src/Snap/Internal/Http/Server/TLS.hs
@@ -133,19 +133,9 @@ bindHttps bindAddress bindPort cert key = do
 
 ------------------------------------------------------------------------------
 freePort :: ListenSocket -> IO ()
-freePort (ListenHttps sock ctx) = do
-    Socket.sClose sock
-    -- FIXME(greg): this is a desperation tactic. Better to find and kill the
-    -- hypothesized liveness bug in HsOpenSSL.
-    --
-    -- Touch the context so that the garbage collector doesn't zap it.
-    !_ <- contextGetCAStore ctx
-    return $! ()
+freePort (ListenHttps sock _) = Socket.sClose sock
 freePort _ = return ()
 
-
-------------------------------------------------------------------------------
-data SessionContext = SessionContext SSL SSLContext
 
 ------------------------------------------------------------------------------
 createSession :: ListenSocket -> Int -> CInt -> IO () -> IO NetworkSession
@@ -154,27 +144,21 @@ createSession (ListenHttps _ ctx) recvSize socket _ = do
     handle (\(e::SomeException) -> Socket.sClose csock >> throwIO e) $ do
         ssl <- connection ctx csock
         accept ssl
-        let sctx = SessionContext ssl ctx
-        return $! NetworkSession socket (unsafeCoerce sctx) recvSize
+        return $! NetworkSession socket (unsafeCoerce ssl) recvSize
 createSession _ _ _ _ = error "can't call createSession on a ListenHttp"
 
 
 ------------------------------------------------------------------------------
 endSession :: NetworkSession -> IO ()
-endSession (NetworkSession _ aSSL _) = do
-    let (SessionContext ssl ctx) = unsafeCoerce aSSL
-    shutdown ssl Unidirectional
+endSession (NetworkSession _ aSSL _) =
+    shutdown (unsafeCoerce aSSL) Unidirectional
 
-    -- FIXME(greg): fix this properly, see above
-    -- Touch the context so that the garbage collector doesn't zap it.
-    !_ <- contextGetCAStore ctx
-    return $! ()
 
 ------------------------------------------------------------------------------
 send :: IO () -> IO () -> NetworkSession -> ByteString -> IO ()
 send tickleTimeout _ (NetworkSession _ aSSL sz) bs = go bs
   where
-    (SessionContext ssl _) = unsafeCoerce aSSL
+    ssl = unsafeCoerce aSSL
 
     -- I think we have to chop the data into chunks here because HsOpenSSL
     -- won't; of course, blaze-builder may already be doing this for us, but I
@@ -195,6 +179,6 @@ recv _ (NetworkSession _ aSSL recvLen) = do
     b <- SSL.read ssl recvLen
     return $! if S.null b then Nothing else Just b
   where
-    (SessionContext ssl _) = unsafeCoerce aSSL
+    ssl = unsafeCoerce aSSL
 
 #endif
