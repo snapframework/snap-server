49533b852651352c56697380df6d8a5e20243952
Gregory Collins <greg@gregorycollins.net>
2013-03-11 19:32:24 +0100

    Add a dummy Paths_snap_server module for testing/ghci.

---
+++ test/Paths_snap_server.hs
@@ -0,0 +1,8 @@
+module Paths_snap_server (
+    version
+  ) where
+
+import           Data.Version (Version (..))
+
+version :: Version
+version = Version {versionBranch = [0,0,0], versionTags = ["unknown"]}
