097eedd33aadf06bc360b67ec8732ac7237131fe
Leonid Onokhov <sopvop@gmail.com>
2015-05-18 13:42:47 +0300

    Document unix socket config options.

--- src/Snap/Internal/Http/Server/Config.hs
+++ src/Snap/Internal/Http/Server/Config.hs
@@ -404,11 +404,21 @@ getSSLChainCert = sslchaincert
 getSSLKey         :: Config m a -> Maybe FilePath
 getSSLKey = sslkey
 
--- | File path to unix socket. Must be absolute.
+-- | File path to unix socket. Must be absolute path, but allows for symbolic
+-- links.
 getUnixSocket     :: Config m a -> Maybe FilePath
 getUnixSocket = unixsocket
 
 -- | Access mode for unix socket, by default is system specific.
+--  This should only be used to grant additional permissions to created
+--  socket file, and not to remove permissions set by default. The only portable way
+--  to limit access to socket is creating it in a directory with proper permissions
+--  set.
+--
+--  Most BSD systems ignore access permissionse on sockets.
+--
+--  Note: This uses umask. There is a race condition if process creates other
+--  files at the same time as opening a unix socket with this option set.
 getUnixSocketAccessMode :: Config m a -> Maybe Int
 getUnixSocketAccessMode = unixaccessmode
 
