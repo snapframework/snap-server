1c9024a4bc2eb54d930e47559bd9a72462cc77ac
Doug Beardsley <mightybyte@gmail.com>
2016-08-06 09:54:25 -0400

    Fix repo mistake

--- deploy_hpc.sh
+++ deploy_hpc.sh
@@ -20,10 +20,13 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
 fi
 
 # Save some useful information
-REPO=`git config remote.origin.url`
-REPO="git@github.com:snapframework/snap-code-coverage"
+REPO="git@github.com:snapframework/snap-code-coverage.git"
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
+
+echo REPO: $REPO
+echo SSH_REPO: $SSH_REPO
+echo SHA: $SHA
 
 # Clone the existing gh-pages for this repo into $OUT/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
