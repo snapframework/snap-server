e81020c48398df8b210ae9718852d5d72e51c5e8
Doug Beardsley <mightybyte@gmail.com>
2016-08-06 10:02:40 -0400

    Need http repo

--- deploy_hpc.sh
+++ deploy_hpc.sh
@@ -20,7 +20,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
 fi
 
 # Save some useful information
-REPO="git@github.com:snapframework/snap-code-coverage.git"
+REPO="https://github.com/snapframework/snap-code-coverage.git"
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
