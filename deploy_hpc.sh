f0083a139007c1fca5440767465fcb339e5fe41f
Greg Hale <imalsogreg@gmail.com>
2016-08-05 22:30:26 -0400

    tweak travis

--- deploy_hpc.sh
+++ deploy_hpc.sh
@@ -55,15 +55,15 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 git add -A
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key_snap_server.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 ~/deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key_snap_server.enc -out deploy_key_snap_server -d
+chmod 600 deploy_key_snap_server
 eval `ssh-agent -s`
-ssh-add ~/deploy_key
+ssh-add deploy_key_snap_server
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
